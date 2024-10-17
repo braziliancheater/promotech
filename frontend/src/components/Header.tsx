a6cd0c32d4697210fc59342fb2de50991f9e5187
brazilian <gabrielelerlima80@gmail.com>
2024-10-17 09:27:23 -0300

    melhorias no header, funcional no mobile agora

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,10 @@
+import {
+  ArrowRight,
+  LucideComputer,
+  LucideDoorOpen,
+  LucideHome,
+  LucideInfo,
+} from "lucide-react";
 import Logo from "../assets/logo.png";
 
 function Header() {
@@ -8,29 +15,42 @@ function Header() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5 mr-6">
+          <a href="/" className="-m-1.5 p-1.5 mr-6">
             <img src={Logo} alt="Logo Promotech" className="h-12 w-18" />
           </a>
 
-          <div className="hidden lg:flex lg:gap-x-12 items-center justify-center">
-            <a href="/" className="font-semibold leading-6 text-gray-900">
+          <div className="flex gap-4 lg:gap-x-12 items-center justify-center">
+            <a
+              href="/"
+              className="flex gap-2 font-semibold leading-6 text-gray-900 hover:text-gray-600"
+            >
+              <LucideHome />
               Incio
             </a>
             <a
               href="/monteseupc"
-              className="font-semibold leading-6 text-gray-900"
+              className="flex gap-2 font-semibold leading-6 text-gray-900 hover:text-gray-600"
             >
+              <LucideComputer />
               Monte seu PC
             </a>
-            <a href="/sobre" className="font-semibold leading-6 text-gray-900">
+            <a
+              href="/sobre"
+              className="flex gap-2 font-semibold leading-6 text-gray-900 hover:text-gray-600"
+            >
+              <LucideInfo />
               Sobre
             </a>
           </div>
         </div>
 
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Entrar <span aria-hidden="true">&rarr;</span>
+          <a
+            href="/login"
+            className="bg-black flex gap-2 font-semibold leading-6 p-2 rounded-lg text-white hover:bg-neutral-700 transition"
+          >
+            Entrar
+            <ArrowRight />
           </a>
         </div>
       </nav>
