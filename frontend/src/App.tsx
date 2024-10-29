846d09106174e2f44cb2107c9979475b98288e59
brazilian <gabrielelerlima80@gmail.com>
2024-10-29 16:09:20 -0300

    adicionado, input box para buscar produtos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
         <Header />
       </div>
 
-      <main className="grid grid-cols-4 gap-4 p-6 ">
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6 ">
         {produtos.map((produto) => (
           <Card
             key={v4()}
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
 import {
   ArrowRight,
   LucideComputer,
-  LucideDoorOpen,
   LucideHome,
   LucideInfo,
+  LucideShoppingCart,
 } from "lucide-react";
 import Logo from "../assets/logo.png";
 
@@ -14,18 +14,67 @@ function Header() {
         className="mx-auto flex items-center justify-between p-4 lg:px-8"
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5 mr-6">
-            <img src={Logo} alt="Logo Promotech" className="h-12 w-18" />
-          </a>
+        <div className="lg:flex-1 grid grid-cols-2 gap-4">
+          <div className="flex items-center gap-4 col-span-2">
+            <a href="/" className="-m-1.5 p-1.5 mr-6 flex items-center">
+              <img src={Logo} alt="Logo Promotech" className="h-12 w-18" />
+              <p className="text-lg font-semibold pl-3">PromoTech</p>
+            </a>
 
-          <div className="flex gap-4 lg:gap-x-12 items-center justify-center">
+            <form>
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="pesquisa"
+                  id="pesquisa"
+                  className="block min-w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Pesquisar"
+                  required
+                />
+              </div>
+            </form>
+
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a
+                href="/login"
+                className="bg-black flex gap-2 font-semibold leading-6 p-2 rounded-lg text-white hover:bg-neutral-700 transition"
+              >
+                Entrar
+                <ArrowRight />
+              </a>
+            </div>
+          </div>
+
+          <div className="flex gap-4 lg:gap-x-12 items-center justify-left">
             <a
               href="/"
               className="flex gap-2 font-semibold leading-6 text-gray-900 hover:text-gray-600"
             >
               <LucideHome />
               Incio
+            </a>
+            <a
+              href="/"
+              className="flex gap-2 font-semibold leading-6 text-gray-900 hover:text-gray-600"
+            >
+              <LucideShoppingCart />
+              Categorias
             </a>
             <a
               href="/monteseupc"
@@ -42,16 +91,6 @@ function Header() {
               Sobre
             </a>
           </div>
-        </div>
-
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a
-            href="/login"
-            className="bg-black flex gap-2 font-semibold leading-6 p-2 rounded-lg text-white hover:bg-neutral-700 transition"
-          >
-            Entrar
-            <ArrowRight />
-          </a>
         </div>
       </nav>
     </header>
