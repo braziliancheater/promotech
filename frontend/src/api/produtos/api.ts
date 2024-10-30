1f1784508c5a28746dea9733b5b77a96dbd31800
brazilian <gabrielelerlima80@gmail.com>
2024-10-30 10:28:09 -0300

    criando arquivo para controle de apis

---
+++ frontend/src/api/produtos/api.ts
@@ -0,0 +1,16 @@
+const API_BASE_URL = "http://localhost:5000/produtos";
+
+export const buscar_produto_por_id = async (id) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}?id=${id}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.produto;
+    } else {
+      console.error("Falha ao obter dados:", response.status);
+    }
+  } catch (error) {
+    console.error("Erro ao buscar produto:", error);
+    throw error;
+  }
+};
