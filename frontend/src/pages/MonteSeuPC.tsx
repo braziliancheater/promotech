616abb00230659c3e838f088c93175448a4d91fb
João Teixeira <brjteixeira@outlook.com>
2024-11-03 13:45:56 -0300

    João Teixeira

--- frontend/src/pages/MonteSeuPC.tsx
+++ frontend/src/pages/MonteSeuPC.tsx
@@ -1,6 +1,34 @@
 import Header from "../components/Header"
+import './MonteSeuPC.css';
 
 function MonteSeuPC() {
+
+    function loadPage(page: string): void {
+        fetch(page)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('ERRO AO CARREGAR PÁGINA');
+                }   
+                return response.text();
+            })
+            .then(data => {
+                const tela = document.getElementById('screens') as HTMLElement | null;
+                if(tela){
+                    tela.innerHTML = data;
+                } else {
+                    console.error("Elemento com o ID 'screens' não encontrado");
+                }
+            })
+            .catch(error => {
+                const tela = document.getElementById('screens') as HTMLElement | null;
+                if(tela){
+                    tela.innerHTML = `<h1>Erro</h1><p>${error.message}</p>`;
+                } else {
+                    console.error("Elemento com o ID 'screens' não encontrado");
+                }
+            });
+    }
+    (window as any).loadPage = loadPage;
 
     return (
         <div>
@@ -11,8 +39,8 @@ function MonteSeuPC() {
             <main className="grid grid-cols-4 gap-4 p-6 ">
                 <div className="menu">
                     <ul>
-                    <li><a href="#" className="parts" data-page="/processadores">PROCESSADORES</a></li>
-                        <li className="parts">teste</li>
+                    <li><a href="#" className="parts" onClick={(event) => {event.preventDefault(); loadPage('/processadores');}}>PROCESSADORES</a></li>
+                        <li><a href="#" className="parts" data-page="#" >teste</a></li>
                         <li className="parts">teste1</li>
                         <li className="parts"></li>
                         <li className="parts"></li>
@@ -24,45 +52,4 @@ function MonteSeuPC() {
     )
 }
 
-function loadPage(page: string): void {
-    fetch(page)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('ERRO AO CARREGAR PÁGINA');
-            }   
-            return response.text();
-        })
-        .then(data => {
-            const tela = document.getElementById('screens') as HTMLElement | null;
-            if(tela){
-                tela.innerHTML = data;
-            } else {
-                console.error("Elemento com o ID 'screens' não encontrado");
-            }
-        })
-        .catch(error => {
-            const tela = document.getElementById('screens') as HTMLElement | null;
-            if(tela){
-                tela.innerHTML = `<h1>Erro</h1><p>${error.message}</p>`;
-            } else {
-                console.error("Elemento com o ID 'screens' não encontrado");
-            }
-        });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const menuItems = document.querySelectorAll('.parts');
-
-    menuItems.forEach(item => {
-        item.addEventListener('click', (event) => {
-            event.preventDefault(); 
-            const page = (event.currentTarget as HTMLElement).getAttribute('data-page');
-            if (page) {
-                loadPage(page);
-            }
-        });
-    });
-});
-
-(window as any).loadPage = loadPage;
 export default MonteSeuPC
