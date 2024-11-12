63aaf1bb2c31762e8f076fd956219fc1033ea377
brazilian <gabrielelerlima80@gmail.com>
2024-11-11 23:16:41 -0300

    colocando site

--- frontend/src/pages/MonteSeuPC/MonteSeuPC.tsx
+++ frontend/src/pages/MonteSeuPC/MonteSeuPC.tsx
@@ -1,69 +1,67 @@
 import { useState, useEffect } from "react";
 
 function MonteSeuPC() {
-  // Define an object to hold the state of all parts
   const [components, setComponents] = useState({
-    processadores: { titulo: "", preco: 0 },
-    placaMae: { titulo: "", preco: 0 },
-    placaVideo: { titulo: "", preco: 0 },
-    memoriaRam: { titulo: "", preco: 0 },
-    armazenamento: { titulo: "", preco: 0 },
-    cooler: { titulo: "", preco: 0 },
-    fonte: { titulo: "", preco: 0 },
+    processadores: { titulo: "", preco: 0, site: "" },
+    placaMae: { titulo: "", preco: 0, site: "" },
+    placaVideo: { titulo: "", preco: 0, site: "" },
+    memoriaRam: { titulo: "", preco: 0, site: "" },
+    armazenamento: { titulo: "", preco: 0, site: "" },
+    cooler: { titulo: "", preco: 0, site: "" },
+    fonte: { titulo: "", preco: 0, site: "" },
   });
 
-  // Function to fetch and parse JSON data from localStorage
   const updateComponent = (key: string) => {
     const novoValor = localStorage.getItem(key);
     if (novoValor) {
       try {
         const parsedData = JSON.parse(novoValor);
-        const titulo = parsedData[0]?.titulo || "Nome não disponível"; // Extract the "titulo" field
-        const preco = parsedData[0]?.preco || 0; // Extract the "preco" field
+        const titulo = parsedData[0]?.titulo || "Nome não disponível";
+        const preco = parsedData[0]?.preco || 0;
+        const site = parsedData[0]?.site || "";
         setComponents((prev) => ({
           ...prev,
-          [key]: { titulo, preco },
+          [key]: { titulo, preco, site },
         }));
       } catch (error) {
         console.error(`Error parsing JSON for ${key}:`, error);
         setComponents((prev) => ({
           ...prev,
-          [key]: { titulo: "Erro ao carregar nome", preco: 0 },
+          [key]: { titulo: "Erro ao carregar nome", preco: 0, site: "" },
         }));
       }
     }
   };
 
-  // Use timer function to keep checking for updates from localStorage
   useEffect(() => {
-    // Timer that updates each component state every second
     const intervalIds = Object.keys(components).map((key) =>
       setInterval(() => updateComponent(key), 1000)
     );
 
     return () => {
-      // Clear the intervals when the component unmounts
       intervalIds.forEach(clearInterval);
     };
   }, [components]);
 
-  // Handle navigation to parts page
   const handleLinkClick = (event: React.MouseEvent, src: string) => {
     event.preventDefault();
     const iframe = document.getElementById("screens") as HTMLIFrameElement;
     iframe.src = src;
   };
 
-  // Calculate total price
   const totalPrice = Object.values(components).reduce((total, component) => {
-    return total + (component.preco || 0);
+    return total + (parseFloat(component.preco) || 0);
   }, 0);
+
+  const formattedTotalPrice = totalPrice.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <main className="flex-grow p-8">
         <div className="max-w-screen-lg mx-auto flex">
-          {/* Menu */}
           <div className="w-1/3 bg-white p-4 shadow-lg rounded-md">
             <ul>
               {Object.keys(components).map((key) => (
@@ -92,7 +90,7 @@ function MonteSeuPC() {
         </div>
 
         <div className="max-w-screen-lg mx-auto mt-8 p-4 bg-white shadow-lg rounded-md">
-          <h2 className="text-2xl font-bold text-center text-blue-800 mb-4">
+          <h2 className="text-2xl font-bold text-center text-black mb-4">
             Resumo do seu PC
           </h2>
           <table className="w-full table-auto border-collapse">
@@ -115,11 +113,15 @@ function MonteSeuPC() {
                   <td className="py-2 px-4 border-b">
                     {key.replace(/([A-Z])/g, " $1").toUpperCase()}
                   </td>
-                  <td className="py-2 px-4 border-b">
-                    {components[key].titulo}
+                  <td className="py-2 px-4 border-b underline text-blue-600">
+                    <a href={components[key].site}>{components[key].titulo}</a>
                   </td>
                   <td className="py-2 px-4 border-b">
-                    {components[key].preco}
+                    {/* Format individual prices */}
+                    {parseFloat(components[key].preco).toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
                   </td>
                 </tr>
               ))}
@@ -131,7 +133,7 @@ function MonteSeuPC() {
                   Total
                 </td>
                 <td className="py-2 px-4 text-lg font-semibold">
-                  {totalPrice}
+                  {formattedTotalPrice}
                 </td>
               </tr>
             </tbody>
